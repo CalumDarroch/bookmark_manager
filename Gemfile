ee71eb856c610516262b789c67c28b453331870e
Calum Darroch <calum_darroch@hotmail.com>
2019-04-23 15:37:24 +0100

    Created model - bookmarks class

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source "https://rubygems.org"
 
 gem 'rake'
+gem 'rspec'
+gem 'rubocop'
+gem 'rubocop-performance'
 gem 'sinatra'
 gem 'capybara'
 gem 'simplecov', require: false
--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require 'sinatra/base'
+require './lib/bookmarks'
 
 class BookmarkManager < Sinatra::Base
   enable :sessions
 
   get '/' do
+    erb :index
   end
-  
+
+  get '/bookmarks' do
+    @bookmark_list = Bookmarks.new.all
+    erb :bookmarks
+  end
+
+  run! if app_file == $0
 end
